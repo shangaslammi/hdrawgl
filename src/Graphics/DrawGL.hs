95808c99db3875c0294e8092a94bedbd89a00d8f
Sami Hangaslammi <shang@iki.fi>
2011-08-24 21:51:39 +0300

    additional Concat instancesW

--- src/Graphics/DrawGL.hs
+++ src/Graphics/DrawGL.hs
@@ -26,19 +26,31 @@ type VertexType = Float
 newtype Vertex = Vertex { fromVertex :: (VertexType, VertexType) }
 newtype Color = Color { fromColor :: (Float,Float,Float,Float) }
 
-class Concat a b where
-    (<++>) :: Concat a b => a -> a -> b
+class Concat a b c where
+    (<++>) :: Concat a b c => a -> b -> c
 
-instance Concat Vertices Vertices where
+instance Concat Vertices Vertices Vertices where
     (<++>) = VertexSum
 
-instance Concat [Vertex] Vertices where
+instance Concat Vertices [Vertex] Vertices where
+    a <++> b = VertexSum a $ VertexList b
+
+instance Concat [Vertex] Vertices Vertices where
+    a <++> b = VertexSum (VertexList a) b
+
+instance Concat [Vertex] [Vertex] Vertices where
     (<++>) = VertexSum `on` VertexList
 
-instance Concat Shape Shape where
+instance Concat Shape Shape Shape where
     (<++>) = TwoShapes
 
-instance Concat [Shape] Shape where
+instance Concat Shape [Shape] Shape where
+    a <++> b = TwoShapes a $ ManyShapes b
+
+instance Concat [Shape] Shape Shape where
+    a <++> b = TwoShapes (ManyShapes a) b
+
+instance Concat [Shape] [Shape] Shape where
     (<++>) = TwoShapes `on` ManyShapes
 
 replaceLeft :: ([a] -> [b]) -> ([c],[a]) -> ([b],[a])
