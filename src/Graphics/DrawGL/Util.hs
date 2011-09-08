531ab75a72b23c65300266b16a694030c5b98f1d
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-09-08 20:57:29 +0300

    added shape bounding box calculation

--- src/Graphics/DrawGL/Util.hs
+++ src/Graphics/DrawGL/Util.hs
@@ -1,6 +1,10 @@
 
 module Graphics.DrawGL.Util where
 
+import Data.Maybe
+
+import Graphics.DrawGL.Fold
+import Graphics.DrawGL.Internal
 import Graphics.DrawGL.Types
 
 fromPolar :: (Angle, Radius) -> Point
@@ -11,3 +15,9 @@ between (min, max) v
     | v < min   = min
     | v > max   = max
     | otherwise = v
+
+boundingBox :: Shape -> (Point, Point)
+boundingBox = fromJust . foldShape step Nothing where
+    step Nothing (Vertex p) = Just (p,p)
+    step (Just ((minx,miny),(maxx,maxy))) (Vertex (x,y)) =
+        Just ((min x minx, min y miny), ((max x maxx, max y maxy)))
