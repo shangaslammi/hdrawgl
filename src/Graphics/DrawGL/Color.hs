380ff08a73fd168b70f755d30ee05ecb4ef8d9b5
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-09-08 20:10:38 +0300

    adding color utilities

--- src/Graphics/DrawGL/Color.hs
+++ src/Graphics/DrawGL/Color.hs
@@ -4,4 +4,17 @@ module Graphics.DrawGL.Color where
 import Graphics.DrawGL.Types
 
 color :: Color -> ColoredVertex -> ColoredVertex
-color c' (c,v) = (c',v)
+color c (_,v) = (c,v)
+
+gradient :: Color -> Color -> (Point -> Float) -> (ColoredVertex -> ColoredVertex)
+gradient a b f (_,v@(Vertex p)) = (mix a b (f p), v)
+
+mix :: Color -> Color -> Float -> Color
+mix a b c = (c' `mult` a) `add` (c `mult` b) where
+    c' = 1.0 - c
+
+add :: Color -> Color -> Color
+add (Color (r,g,b,a)) (Color (r',g',b',a')) = Color ((r+r'),(g+g'),(b+b'),(a+a'))
+
+mult :: Float -> Color -> Color
+mult v (Color (r,g,b,a)) = Color ((r*v),(g*v),(b*v),(a*v))
